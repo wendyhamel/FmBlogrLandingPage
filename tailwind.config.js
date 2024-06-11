9ecdf72bba235a84c0932a09a05650ccacf36bee
wendyhamel <whamel@it4web.nl>
2024-06-11 17:36:06 +0200

    Setup files and started on styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Overpass', 'sans-serif'],
+      'heading': ['Ubuntu', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'light-red' : 'hsl(356, 100%, 66%)', //cta text
+        'red' : 'hsl(355, 100%, 74%)', //cta hover background
+        'dark-blue' : 'hsl(208, 49%, 24%)', //headings
+        'gray-blue' : 'hsl(240, 2%, 79%)', //footer text
+        'dark-gray-blue' : 'hsl(207, 13%, 34%)', //body copy
+        'dark-black-blue' : 'hsl(240, 10%, 16%)', //footer background
+
+        'red-gradient-start' : 'hsl(13, 100%, 72%)', //intro/cta mobile nav
+        'red-gradient-end' : 'hsl(353, 100%, 62%)',
+
+        'blue-gradient-start' : 'hsl(237, 17%, 21%)', //body
+        'blue-gradient-end' : 'hsl(237, 23%, 32%)',
+      }
+    },
   },
   plugins: [],
 }
